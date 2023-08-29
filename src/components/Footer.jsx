18600bd615f2af96b60517b37e6fcc75d36e6741
sami12344 <freelancersami16203@gmail.com>
2023-08-29 15:02:34 +0600

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,108 @@
-import React from "react";
-import { TextField } from "./Fields";
-import Button from "./Button";
-import Container from "./Container";
+'use client'
+
+import React from 'react';
+import { motion } from 'framer-motion';
+import {
+  FaFacebook,
+  FaTwitter,
+  FaLinkedin,
+  FaInstagram,
+  FaYoutube,
+} from 'react-icons/fa';
+import ScrollTrigger from 'react-scroll-trigger';
+import Logo from './Logo';
 
 const Footer = () => {
+  const footerVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+  };
+
   return (
-    <footer>
-      <Container>
-        <div className="flex flex-col items-center border-t border-gray-200 pb-12 pt-8 md:flex-row md:justify-between md:pt-6">
-          <p className="text-xs text-gray-500">
-            &copy; Copyright {new Date().getFullYear()}. All rights reserved.
-          </p>
-          <form className="flex w-full justify-center md:w-auto mt-6 md:mt-0">
-            <TextField
-              type="email"
-              aria-label="Email address"
-              placeholder="Email address"
-              autoComplete="email"
-              required
-              className="w-60 min-w-0 shrink"
-            />
-            <Button type="submit" color="blue" className="ml-4 flex-none">
-              <span className="hidden lg:inline">Join our newsletter</span>
-              <span className="lg:hidden">Join newsletter</span>
-            </Button>
-          </form>
+    <ScrollTrigger onEnter={() => console.log('Footer in view')}>
+      <motion.footer
+        className='bg-gray-800 text-white py-10'
+        variants={footerVariants}
+        initial='hidden'
+        animate='visible'
+      >
+        <div className='container mx-auto grid md:grid-cols-2 lg:grid-cols-5 gap-8 px-6'>
+          <div className='col-span-1 md:col-span-2 lg:col-span-1'>
+            <div className='flex items-center mb-4'>
+             <Logo />
+         
+            </div>
+            <p className='text-sm'>
+              Trustworthy futuristic investment for long-term well-being.
+            </p>
+          </div>
+          <div className='col-span-1 md:col-span-2 lg:col-span-1'>
+            <h3 className='text-lg font-semibold mb-3'>Contact Us</h3>
+            <p className='text-sm mb-2'>Email: contact@investa.com</p>
+            <p className='text-sm mb-2'>Phone: +123 456 7890</p>
+            <p className='text-sm mb-2'>Address: 123 Main St, City</p>
+            <p className='text-sm mb-2'>Postcode: 12345</p>
+          </div>
+          <div className='col-span-1 md:col-span-2 lg:col-span-1'>
+            <h3 className='text-lg font-semibold mb-3'>Page Links</h3>
+            <ul className='text-sm space-y-2'>
+              <li>
+                <a href='#'>Home</a>
+              </li>
+              <li>
+                <a href='#'>About Us</a>
+              </li>
+              <li>
+                <a href='#'>Services</a>
+              </li>
+              <li>
+                <a href='#'>Contact</a>
+              </li>
+              <li>
+                <a href='#'>Privacy Policy</a>
+              </li>
+              <li>
+                <a href='#'>Terms of Use</a>
+              </li>
+            </ul>
+          </div>
+          <div className='col-span-1 md:col-span-2 lg:col-span-1'>
+            <h3 className='text-lg font-semibold mb-3'>Follow Us</h3>
+            <div className='flex space-x-3'>
+                     <FaFacebook className="text-2xl" />
+            <FaTwitter className="text-2xl" />
+            <FaLinkedin className="text-2xl" />
+            <FaInstagram className="text-2xl" />
+            <FaYoutube className="text-2xl" />
+
+              {/* Other social links */}
+            </div>
+          </div>
+          <div className='col-span-2 lg:col-span-1'>
+            <h3 className='text-lg font-semibold mb-3'>Join Our Newsletter</h3>
+            <form className='flex space-x-0'>
+              <input
+                type='email'
+                className='border rounded-l px-3 py-2 w-full focus:outline-none'
+                placeholder='Your email'
+              />
+              <button
+                type='submit'
+                className='bg-blue-500 text-white rounded-r px-4 py-2 focus:outline-none'
+              >
+                Subscribe
+              </button>
+            </form>
+          </div>
         </div>
-      </Container>
-    </footer>
+        <div className='container mx-auto mt-6 text-center'>
+          <hr className='border-gray-300 mb-3' />
+          <p className='text-gray-400'>
+            &copy; {new Date().getFullYear()} Investa. All rights reserved.
+          </p>
+        </div>
+      </motion.footer>
+    </ScrollTrigger>
   );
 };
 
