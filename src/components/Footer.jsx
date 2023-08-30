b4fd382631eeb05186e1e3c9ed1405f822675db8
sami12344 <freelancersami16203@gmail.com>
2023-08-30 19:42:52 +0600

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -26,49 +26,55 @@ const Footer = () => {
         initial='hidden'
         animate='visible'
       >
-        <div className='container mx-auto grid md:grid-cols-2 lg:grid-cols-5 gap-8 px-6'>
+        <div className='container mx-auto grid md:grid-cols-2 lg:grid-cols-5 gap-8 px-12'>
           <div className='col-span-1 md:col-span-2 lg:col-span-1'>
             <div className='flex items-center mb-4'>
-              <Logo className='text-gray-200 hover:text-white' />
+              <Logo className='text-gray-100 hover:text-white' />
             </div>
-            <p className='text-sm'>
+            <p className='text-sm text-gray-400'>
               &ldquo; Empowering Your Financial Future: Discover Prosperity with
               Investa&apos;s Secure and Innovative Investment Solutions. &rdquo;
             </p>
           </div>
           <div className='col-span-1 md:col-span-2 lg:col-span-1'>
-            <h3 className='text-lg font-semibold mb-3'>Contact Us</h3>
-            <p className='text-sm mb-2'>Email: contact@investa.com</p>
-            <p className='text-sm mb-2'>Phone: +123 456 7890</p>
-            <p className='text-sm mb-2'>Address: 123 Main St, City</p>
-            <p className='text-sm mb-2'>Postcode: 12345</p>
+            <h3 className='text-lg font-semibold mb-3 text-gray-200'>
+              Contact Us
+            </h3>
+            <p className='text-sm mb-2 text-gray-400'>
+              Email: contact@investa.com
+            </p>
+            <p className='text-sm mb-2 text-gray-400'>Phone: +123 456 7890</p>
+            <p className='text-sm mb-2 text-gray-400'>
+              Address: 123 Main St, City
+            </p>
+            <p className='text-sm mb-2 text-gray-400'>Postcode: 12345</p>
           </div>
           <div className='col-span-1 md:col-span-2 lg:col-span-1'>
             <h3 className='text-lg font-semibold mb-3'>Page Links</h3>
             <ul className='text-sm space-y-2'>
-              <li>
+              <li className='text-gray-400'>
                 <a href='#'>Home</a>
               </li>
-              <li>
+              <li className='text-gray-400'>
                 <a href='#'>About Us</a>
               </li>
-              <li>
+              <li className='text-gray-400'>
                 <a href='#'>Services</a>
               </li>
-              <li>
+              <li className='text-gray-400'>
                 <a href='#'>Contact</a>
               </li>
-              <li>
+              <li className='text-gray-400'>
                 <a href='#'>Privacy Policy</a>
               </li>
-              <li>
+              <li className='text-gray-400'>
                 <a href='#'>Terms of Use</a>
               </li>
             </ul>
           </div>
           <div className='col-span-1 md:col-span-2 lg:col-span-1'>
             <h3 className='text-lg font-semibold mb-3'>Follow Us</h3>
-            <div className='flex space-x-3'>
+            <div className='flex space-x-3 text-gray-400'>
               <FaFacebook className='text-2xl' />
               <FaTwitter className='text-2xl' />
               <FaLinkedin className='text-2xl' />
@@ -88,7 +94,7 @@ const Footer = () => {
               />
               <button
                 type='submit'
-                className='bg-blue-500 text-white rounded-r px-4 py-2 focus:outline-none'
+                className='bg-blue-600 text-white rounded-r px-4 py-2 focus:outline-none'
               >
                 Subscribe
               </button>
